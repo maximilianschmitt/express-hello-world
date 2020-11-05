4eb793f18a28d9165894a9a3b6d439c1638f9388
Maximilian Schmitt <maximilian.schmitt@googlemail.com>
2020-11-06 00:49:45 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const PORT = process.env.PORT || 4000
+
+const app = express()
+
+app.get('/', (req, res) => {
+	res.send('Hello world')
+})
+
+app.get('/:message', (req, res) => {
+	res.send('Your message: ' + req.params.message)
+})
+
+app.listen(PORT, (err) => {
+	if (err) {
+		throw err
+	}
+
+	console.log('Express server listening on port ' + PORT)
+})
